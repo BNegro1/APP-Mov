6301b0c78fb4101095d8d3e8a05bab3e011016b2
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-08-31 19:06:51 -0400

    Implementación y modificacion funciones

--- Unidad 1/Ejemplos/RegistraAPP/src/app/login/login.page.ts
+++ Unidad 1/Ejemplos/RegistraAPP/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {
   }
 
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: 'Ingreso exitoso!',
+      duration: 2000,
+      color: 'success',
+      animated: true,
+    });
+    toast.present();
+  }
 }
