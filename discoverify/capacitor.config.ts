0a73da0b3a740ed31292729ab851d206feb7c969
Wal-Wandering <72948318+BNegro1@users.noreply.github.com>
2024-09-28 00:06:14 -0300

    Elimina discoverify directory

--- discoverify/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'discoverify',
-  webDir: 'www'
-};
-
-export default config;
