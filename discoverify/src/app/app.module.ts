6955c95b65b729d44ad2cc9c3b88a9259f40cd6f
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-10-04 17:35:40 -0300

    Actualiza modulos Spotify

--- discoverify/src/app/app.module.ts
+++ discoverify/src/app/app.module.ts
@@ -7,9 +7,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SpotifyModule } from 'angular-spotify'; // Modulo de spotify (discoverify\src\angular-spotify.d.ts)
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+
+    // Config del módulo de Spotify
+    // Cliente secret: 90bdd6d2ad06437185aa0eeda7bb1fc8
+    // Client id: 3881fb07520a491f9090ad3ebc97ccb8
+    // Redirect URIs (En caso de error, sucecs, failrule, etc): http://localhost:8100/errorview
+    
+    SpotifyModule.forRoot({
+      clientId: 'ABC123DEF456GHI789JKL', // Cambiar por el client id de la app de Spotify.
+      redirectUri: 'http://www.example.com/callback.html',
+      scope:
+        'user-read-private playlist-read-private playlist-modify-private playlist-modify-public',
+      showDialog: true,
+      authToken: 'zoasliu1248sdfuiknuha7882iu4rnuwehifskmkiuwhjg23',
+    }),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
