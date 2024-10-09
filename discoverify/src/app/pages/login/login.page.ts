e0aedcb512ba76ad5b5d0b8a2fadac3edfe00053
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-10-09 09:10:18 -0300

    Implementa register y login
    - Implementar toast o alternativas.
    - Corregir flujo de datos correctamente.

--- discoverify/src/app/pages/login/login.page.ts
+++ discoverify/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  // Modelo completo para el login: User y Password.
+  login: any = {
+    email: '',
+    contrasenna: '',
+  };
 
-  constructor() { }
+  // Variables para ambos inputs cuando estén vacios.
+  campoCorreo: string = '';
+  campoContrasenna: string = '';
 
-  ngOnInit() {
+  constructor(public router: Router) {}
+
+  ngOnInit() {}
+  ingresar() {
+    if (this.validarDatos(this.login)) {
+      // Creación de parámetros, misma forma: "{ state: {login: this.login} };"
+      let navigationExtras: NavigationExtras = {
+        state: { userEmail: this.login.email },
+      }; // Rescatamos el email para guardarlo y mostrarlo en page home.
+      this.router.navigate(['/home'], navigationExtras);
+     
+    }
+  }
+  
+  /**
+   * validarDatos para validar el ingreso de algo en los
+   * campos de mi html mediante el modelo login
+   */
+  validarDatos(model: any): boolean {
+
+    // Validación del correo electrónico
+    const emailArroba = '@';
+    const emailDuoc = 'duocuc.cl';
+
+    if (!model.email || !model.email.includes(emailArroba) || !model.email.includes(emailDuoc)) {
+      this.campoCorreo = "email";
+      return false;
+    }
+
+    // Validación de la contrasenna (Contraseña)
+    // !!! La contraseña debe ser "admin" o "123" !!!
+    if (!model.contrasenna || !(model.contrasenna === 'admin' || model.contrasenna === '123')) {
+      this.campoContrasenna = "contrasenna";
+      return false;
+    }
+
+    // Reinicio de errores.
+    this.campoCorreo = "";
+    this.campoContrasenna = "";
+    return true;
   }
 
 }
+
--- discoverify/src/app/pages/register/register.page.ts
+++ discoverify/src/app/pages/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  // Modelo completo para el registro: User y Password.
+  register: any = {
+    email: '',
+    contrasenna: '',
+  };
 
-  constructor() { }
+  // Variables para ambos inputs cuando estén vacios.
+  campoCorreo: string = '';
+  campoContrasenna: string = '';
 
-  ngOnInit() {
+  constructor(public router: Router) {}
+
+  ngOnInit() {}
+
+  registrar() {
+    if (this.validarDatos(this.register)) {
+      // Creación de parámetros, misma forma: "{ state: {register: this.register} };"
+      let navigationExtras: NavigationExtras = {
+        state: { userEmail: this.register.email },
+      }; // Rescatamos el email para guardarlo y mostrarlo en page home.
+      this.router.navigate(['/home'], navigationExtras);
+    }
   }
 
+  /**
+   * validarDatos para validar el ingreso de algo en los
+   * campos de mi html mediante el modelo register
+   */
+  validarDatos(model: any): boolean {
+    // Validación del correo electrónico
+    const emailArroba = '@';
+    const emailDuoc = 'duocuc.cl';
+
+    if (
+      !model.email ||
+      !model.email.includes(emailArroba) ||
+      !model.email.includes(emailDuoc)
+    ) {
+      this.campoCorreo = 'email';
+      return false;
+    }
+
+    // Validación de la contrasenna (Contraseña)
+    // !!! La contraseña debe ser "admin" o "123" !!!
+    if (
+      !model.contrasenna ||
+      !(model.contrasenna === 'admin' || model.contrasenna === '123')
+    ) {
+      this.campoContrasenna = 'contrasenna';
+      return false;
+    }
+
+    // Reinicio de errores.
+    this.campoCorreo = '';
+    this.campoContrasenna = '';
+    return true;
+  }
 }
