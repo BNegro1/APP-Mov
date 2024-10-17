8bcd4c109267554f2625496229c2d2260c8e54e3
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-10-17 13:03:16 -0300

    Update auth.guard.ts

--- discoverify/src/guards/auth.guard.ts
+++ discoverify/src/guards/auth.guard.ts
@@ -1,4 +1,6 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router  } from '@angular/router';
+import { Injectable } from '@angular/core'
+import { DbService } from '../services/dbservice.service'; // Fixear !!!
 
 export const authGuard: CanActivateFn = (route, state) => {
   return true;
