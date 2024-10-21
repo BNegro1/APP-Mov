94070b3b6af143fdceaa77ccc21286b350f2ec7b
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-10-21 17:24:33 -0300

    Update auth.guard.ts

--- discoverify/src/app/services/auth.guard.ts
+++ discoverify/src/app/services/auth.guard.ts
@@ -6,12 +6,20 @@ import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
+
+// UrlTree es un tipo de Angular que representa una URL o un árbol de URL
+// Por qué? Porque la función canActivate() puede devolver un booleano o un UrlTree
+// Si devuelve un booleano, la navegación se permite o se bloquea.
+
+// Investigar más, aunque con esto basta mietras (?)
+
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree { // Comprueba si el usuario está autenticado
+      // Explicación breve: si el usuario está autenticado, devuelve true; si no, redirige al usuario a la página de login
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
