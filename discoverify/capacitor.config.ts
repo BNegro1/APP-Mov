02359d4f514b7ce254a3c4db615be0ccba841082
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-12-01 01:26:18 -0300

    Final dev

--- discoverify/capacitor.config.ts
+++ discoverify/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Discoverify',
-  webDir: 'www'
+  webDir: 'www/browser'
 };
 
 export default config;
