3b201606722c8da20557592ce3428408c9dc00b5
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-12-01 01:53:29 -0300

    finaliza deploy - views - sign

--- discoverify/capacitor.config.ts
+++ discoverify/capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'ds.discovery.fy',
   appName: 'Discoverify',
-  webDir: 'www'
+  webDir: 'www/browser'
 };
 
 export default config;
